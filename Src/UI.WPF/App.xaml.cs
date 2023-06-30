6f7ff9f30c139161f6e47e7aa3fa51a466ec5515
Gorddd <kasual2014@icloud.com>
2023-06-30 18:54:12 +0300

    init

---
+++ Src/UI.WPF/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using UI.WPF.Views;
+
+namespace UI.WPF;
+
+public partial class App : Application
+{
+    private readonly IHost host;
+
+    public App()
+    {
+        var builder = Host.CreateDefaultBuilder();
+
+        host = builder.ConfigureServices(services =>
+        {
+            services.AddSingleton<MainWindow>();
+        }).Build();
+    }
+}
